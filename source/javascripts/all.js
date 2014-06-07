4f5eb123293776076ba6ed7d25eb6b91f587d3b6
Neson <neson@dex.tw>
2014-06-08 07:57:36 +0800

    update

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,7 @@
 //= require jquery/dist/jquery.js
 //= require fastclick/lib/fastclick.js
 //= require foundation/js/foundation.js
-//= require baseline.js
+//= require smoothScrolling.js
 //= require jquery.lazyload/jquery.lazyload
 //= include app.js
 
