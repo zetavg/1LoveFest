d0d5c0a08ae5dc007ac2ce59d28002908c811da1
Neson <neson@dex.tw>
2014-07-02 21:28:42 +0800

    update

--- config.rb
+++ config.rb
@@ -67,7 +67,7 @@ configure :build do
   # Minify Javascript on build
   activate :minify_javascript
 
-  activate :imageoptim
+  # activate :imageoptim
 
   # Enable cache buster
   # activate :asset_hash
